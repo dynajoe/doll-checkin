625d30fb1c55aac71553caa9d0e3b0b516a2ccdf
Joe Andaverde <joe@andaverde.net>
2012-09-04 15:27:44 -0500

    Accidentally added incorrect output

--- checkin.ps1
+++ checkin.ps1
@@ -112,8 +112,6 @@ if ($imprint -ne '')
       msiexec /l*v install.log /i "c:\latestinstaller.msi" $env:CI_MSI_PARAMETERS.Split(" ") /quiet | add-content "c:\updatelog.txt"
     }
     
-    $!
-    $_
     if ($?) {
       Report $checkin_url $active $imprint 'imprint' 'success'  
     } else {
